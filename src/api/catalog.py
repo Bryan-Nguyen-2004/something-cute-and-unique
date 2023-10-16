b660d730aeb6f56aaca36f41b2b71ede06156009
Bryan Nguyen <bryancollege71@gmail.com>
2023-10-16 10:16:47 -0700

    v 3.11 - limit catalog to 6 items

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
+from sqlalchemy.exc import DBAPIError
 
 router = APIRouter()
 
@@ -11,27 +12,32 @@ def get_catalog():
     """
     catalog = []
 
-    with db.engine.begin() as connection:
-        # query catalog for items in stock
-        result = connection.execute(
-            sqlalchemy.text(
-                """
-                SELECT sku, name, stock AS quantity, price, ARRAY[red_ml, green_ml, blue_ml, dark_ml] AS potion_type
-                FROM catalog
-                WHERE stock > 0
-                """
-            ))
-        
-        # create catalog with queried quantities
-        for sku, name, quantity, price, potion_type in result:
-            item = {
-                "sku": sku,
-                "name": name,
-                "quantity": quantity,
-                "price": price,
-                "potion_type": potion_type
-            }
-            catalog.append(item)
+    try: 
+        with db.engine.begin() as connection:
+            # query catalog for items in stock
+            result = connection.execute(
+                sqlalchemy.text(
+                    """
+                    SELECT sku, name, stock AS quantity, price, ARRAY[red_ml, green_ml, blue_ml, dark_ml] AS potion_type
+                    FROM catalog
+                    WHERE stock > 0
+                    ORDER BY stock DESC
+                    LIMIT 6
+                    """
+                ))
+            
+            # create catalog with queried quantities
+            for sku, name, quantity, price, potion_type in result:
+                item = {
+                    "sku": sku,
+                    "name": name,
+                    "quantity": quantity,
+                    "price": price,
+                    "potion_type": potion_type
+                }
+                catalog.append(item)
+    except DBAPIError as error:
+        print(f"Error returned: <<<{error}>>>")
 
     print("catalog:", catalog)
 
